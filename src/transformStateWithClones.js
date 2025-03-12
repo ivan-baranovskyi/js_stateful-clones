eca6d721ddfca42605b98de7e1fb7ac05b5e411a
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2025-03-12 11:40:20 +0100

    completed task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateClone = { ...state };
+  const result = [];
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      const value = action.extraData;
+
+      for (const key in value) {
+        stateClone[key] = value[key];
+      }
+      result.push({ ...stateClone });
+    } else if (action.type === 'removeProperties') {
+      for (const toRemove of action.keysToRemove) {
+        delete stateClone[toRemove];
+      }
+      result.push({ ...stateClone });
+    } else if (action.type === 'clear') {
+      for (const key in stateClone) {
+        delete stateClone[key];
+      }
+      result.push({ ...stateClone });
+    }
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
